9501d075cb94a0400ccce108f826de89f17f1bf1
daniel <13wrightd@gmail.com>
2015-08-09 17:45:13 -0400

    deploy

--- git.php
+++ git.php
@@ -15,5 +15,6 @@ file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 if ($payload->ref === 'refs/heads/master')
 {
   // path to your site deployment script
-  exec('./deploy.sh');
+  shell_exec('node app.js');  
+//exec('./deploy.sh');
 }
