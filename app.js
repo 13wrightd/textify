5a182709913bd1a5b833948160d2a75a183a0cab
daniel <13wrightd@gmail.com>
2015-08-09 17:14:12 -0400

    deploy script

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/git.php', function(req, res) {
+  res.sendFile(__dirname+'/git.php');
+});
+
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git pull github master
+node ./app.js
